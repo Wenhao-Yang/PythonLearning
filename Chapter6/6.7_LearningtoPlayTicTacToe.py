4339b889c80196909471afe75a7db7277dd39340
yangwenhao Win10 Lab <874581044@qq.com>
2019-02-26 20:25:50 +0800

    6.6
    Signed-off-by: yangwenhao Win10 Lab <874581044@qq.com>

---
+++ Chapter6/6.7_LearningtoPlayTicTacToe.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+@Author: yangwenhao
+@Contact: 874681044@qq.com
+@Software: PyCharm
+@File: 6.7_LearningtoPlayTicTacToe.py
+@Time: 2019/2/25 10:42
+@Overview: In the recipe, we will try to use a neural network to learn the optimal response for a number of different boards. The reason is that the game is a deterministic game and the optimal moves are already known.
+"""
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
