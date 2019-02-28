be6aadddc0de8220488f720416f4b76850e3ac57
yangwenhao Win10 Lab <874581044@qq.com>
2019-02-28 20:42:57 +0800

    7.1
    Signed-off-by: yangwenhao Win10 Lab <874581044@qq.com>

--- Chapter7 Natural Language Processing/7.1_WorkingWithBagOfWords.py
+++ Chapter7 Natural Language Processing/7.1_WorkingWithBagOfWords.py
@@ -22,8 +22,8 @@ from tensorflow.contrib import learn
 sess = tf.Session()
 
 save_file_name = os.path.join('../', 'LocalData/temp_spam_data.csv')
-if os.path.isfile(save_file_name) & os.path.getsize(save_file_name) >0 :
-    text_data = []
+text_data = []
+if os.path.isfile(save_file_name) & os.path.getsize(save_file_name)>0 :
     with open(save_file_name, 'r') as temp_output_file:
         reader = csv.reader(temp_output_file)
         for row in reader:
@@ -46,27 +46,38 @@ else:
         writer = csv.writer(temp_output_file)
         writer.writerow(text_data)
 
-    texts = [x[1] for x in  text_data]
-    target = [x[0] for x in text_data]
+texts = [x[1] for x in  text_data]
+target = [x[0] for x in text_data]
 
-    # Relabel s[am as 1, ham as 0
-    target = [1 if x=='spam' else 0 for x in target]
+# Relabel s[am as 1, ham as 0
+target = [1 if x=='spam' else 0 for x in target]
 
-    # Normalize the text
-    texts = [x.lower() for x in texts]
-    # Remove punctuation
-    texts = [''.join(c for c in x if c not in string.punctuation) for x in texts]
-    # Remove numbers
-    texts = [''.join(c for c in x if c not in '0123456789') for x in texts]
-    # Trim extra whitespace
-    texts = [' '.join(x.split()) for x in texts]
+# Normalize the text
+texts = [x.lower() for x in texts]
+# Remove punctuation
+texts = [''.join(c for c in x if c not in string.punctuation) for x in texts]
+# Remove numbers
+texts = [''.join(c for c in x if c not in '0123456789') for x in texts]
+# Trim extra whitespace
+texts = [' '.join(x.split()) for x in texts]
 
-    # Plot histogram of text lengths
-    text_lengths = [len(x.split()) for x in texts]
-    text_lengths = [x for x in text_lengths if x<50]
-    plt.hist(text_lengths, bins=25)
-    plt.title('Histogram of # of Words in Texts')
-    plt.show()
+# Plot histogram of text lengths
+text_lengths = [len(x.split()) for x in texts]
+text_lengths = [x for x in text_lengths if x<50]
+plt.hist(text_lengths, bins=25)
+plt.title('Histogram of # of Words in Texts')
+plt.show()
     
-    sentence_size = 25
-    min_word_freq = 3
+sentence_size = 25
+min_word_freq = 3
+
+# TensorFlow has a built-in processing tool for determining vocabulary , called VocabularyProcessor().
+vocab_processor = learn.preprocessing.VocabularyProcessor(sentence_size=sentence_size, min_frequency=min_word_freq)
+vocab_processor.fit_transform(texts)
+embedding_size = len(vocab_processor.vocabulary_)
+
+# Split the data into train and test dataset
+train_indcies = np.random.choice(len(texts), round(len(texts)*0.8), replace=False)
+
+
+
